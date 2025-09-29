839f0d51aaa1d465815db63723ea98f2ac1d1fc7
HussR98 <83137889+HussR98@users.noreply.github.com>
2025-09-29 15:45:48 -0400

    Add Dockerfile and k8s resources

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,5 +1,3 @@
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
 import Clicker from './Clicker'
 import UpgradePanel from './UpgradePanel'
 import { upgrades } from '../utils/upgrades/upgrades'
